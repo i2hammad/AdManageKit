20b7f820de8d80a1877c4d6cc5a025010f40a09e
Muhammad Hammad <Hammadmust2013@gmail.com>
2025-12-13 19:23:32 +0500

    Remove gma-next-gen-sdk-android-examples-main from repo (reference only)

--- gma-next-gen-sdk-android-examples-main/java/nextgenexample/app/src/main/java/com/example/nextgenexample/inlinebanner/FoodMenuItem.java
+++
@@ -1,5 +0,0 @@
-package com.example.nextgenexample.inlinebanner;
-
-/** The [FoodMenuItem] class. Defines the attributes for a restaurant menu item. */
-public record FoodMenuItem(
-    String name, String description, String price, String category, String imageName) {}
